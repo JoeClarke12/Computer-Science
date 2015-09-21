ceda9444a9c6ee0e25daf44bc24330ceb2ec9761
JoeClarke12 <joe.clarke319@outlook.com>
2015-09-21 13:34:16 +0100

    Update Arithmetic quiz.py

--- Arithmetic quiz.py
+++ Arithmetic quiz.py
@@ -4,6 +4,20 @@ import random
 question = 1
 number1 = 1
 number2 = 1
+o = 1
+score = 0
+
+def add(x, y):
+
+   return x + y
+
+def subtract(x, y):
+
+   return x - y
+
+def divide(x, y):
+
+   return x / y
 
 NAME = input("Hello, \nWhat is your name? ")
 print("Welcome to this maths quiz", NAME, "Good luck")
@@ -13,11 +27,36 @@ time.sleep(1)
 while question <= 10:
     number1 = random.randint(0, 101)
     number2 = random.randint(0, 101)
-    print(number1, "+", number2)
+    operator = random.randint(1, 3)
+    if operator == 1:
+       print(number1,"+",number2)
+    elif operator == 2:
+      print(number1,"-",number2)
+    elif operator == 3:
+      print(number1,"/",number2)
+
+    if operator == 1:
+       ans = add(number1,number2)
+    elif operator == 2:
+      ans = subtract(number1,number2)
+    elif operator == 3:
+      ans = divide(number1,number2)
+      
+
     answer = int(input())
-    if answer == number1 + number2:
+    
+    if answer == ans:
         print("correct")
+        score = score + 1
     else:
-        print("false")
+        print("false, the answer was actually", ans)
+        
 
     question = question + 1
+
+
+if score < 5:
+   print("Unfortunatly your score was", score)
+
+else:
+   print("Amazing, your score was", score)
